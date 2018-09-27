115ab9732639c6e4bd890e0a756c40d4c422cc8b
Stefal <stephane.peneau@wanadoo.fr>
2018-09-27 17:12:58 +0200

    Creating a class to control the Yi2k

--- raspberry/Yi2k_scripts/_4Yi_set_clock_async.py
+++ raspberry/Yi2k_scripts/_4Yi_set_clock_async.py
@@ -7,101 +7,55 @@ import os, re, sys, time, socket, asyncio
 from settings import camaddr, cam1addr, cam2addr, cam3addr, cam4addr
 from settings import camport, cam1port, cam2port, cam3port, cam4port
 from time import localtime, strftime
-#srv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-srv1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-srv2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-srv3 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-srv4 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#srv.connect((camaddr, camport))
-srv1.connect((cam1addr, cam1port))
-srv2.connect((cam2addr, cam2port))
-srv3.connect((cam3addr, cam3port))
-srv4.connect((cam4addr, cam4port))
-
-#srv.send('{"msg_id":257,"token":0}')
-srv1.send('{"msg_id":257,"token":0}'.encode())
-srv2.send('{"msg_id":257,"token":0}'.encode())
-srv3.send('{"msg_id":257,"token":0}'.encode())
-srv4.send('{"msg_id":257,"token":0}'.encode())
-"""
-data = srv.recv(512)
-if "rval" in data:
-    token = re.findall('"param": (.+) }',data)[0]   
-else:
-    data = srv.recv(512)
-    if "rval" in data:
-        token = re.findall('"param": (.+) }',data)[0]   
-
-"""
-data1 = srv1.recv(512).decode()
-if "rval" in data1:
-    token1 = re.findall('"param": (.+) }',data1)[0] 
-else:
-    data1 = srv1.recv(512).decode()
-    if "rval" in data1:
-        token1 = re.findall('"param": (.+) }',data1)[0] 
-
-
-data2 = srv2.recv(512).decode()
-if "rval" in data2:
-    token2 = re.findall('"param": (.+) }',data2)[0] 
-else:
-    data2 = srv2.recv(512).decode()
-    if "rval" in data2:
-        token2 = re.findall('"param": (.+) }',data2)[0] 
-
-data3 = srv3.recv(512).decode()
-if "rval" in data3:
-    token3 = re.findall('"param": (.+) }',data3)[0] 
-else:
-    data3 = srv3.recv(512).decode()
-    if "rval" in data3:
-        token3 = re.findall('"param": (.+) }',data3)[0] 
-
-data4 = srv4.recv(512).decode()
-if "rval" in data4:
-    token4 = re.findall('"param": (.+) }',data4)[0] 
-else:
-    data4 = srv4.recv(512).decode()
-    if "rval" in data4:
-        token4 = re.findall('"param": (.+) }',data4)[0] 
-
-
-# Ca fonctionne :
-#tosend = '{"msg_id":2,"token":%s, "type":"camera_clock", "param":"2011-01-03 03:57:16"}' %token
-#srv.send(tosend)
-#srv.recv(512)
-myLocTime=strftime("%Y-%m-%d %H:%M:%S", localtime())
-start_time = time.time()
-
-#tosend = '{"msg_id":2,"token":%s, "type":"camera_clock", "param":"' %token + str(myLocTime) + '"}'
-tosend1 = '{"msg_id":2,"token":%s, "type":"camera_clock", "param":"' %token1 + str(myLocTime) + '"}'
-tosend2 = '{"msg_id":2,"token":%s, "type":"camera_clock", "param":"' %token2 + str(myLocTime) + '"}'
-tosend3 = '{"msg_id":2,"token":%s, "type":"camera_clock", "param":"' %token3 + str(myLocTime) + '"}'
-tosend4 = '{"msg_id":2,"token":%s, "type":"camera_clock", "param":"' %token4 + str(myLocTime) + '"}'
-
-
-#srv.send(tosend)
-#srv.recv(512)
-
-srv1.send(tosend1.encode())
-srv2.send(tosend2.encode())
-srv3.send(tosend3.encode())
-srv4.send(tosend4.encode())
-srv1.recv(512)
-srv2.recv(512)
-srv3.recv(512)
-srv4.recv(512)
-
-srv1.close()
-srv2.close()
-srv3.close()
-srv4.close()
-
-
-
-print("Time sets to {}".format(myLocTime))
-total_time = time.time() - start_time
-print("temps écoulé : {}".format(total_time))
+class Yi2K_telnet (object):
+    def __init__ (self, cam_address, cam_port = 7878):
+        self.cam_address = cam_address
+        self.cam_socket = None
+        self.cam_port = cam_port
+        self.cam_token = None
+        self._isconnected = False
+        
+    def connect(self):
+        self.cam_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.cam_socket.connect((self.cam_address, self.cam_port))
+        self.cam_socket.send('{"msg_id":257,"token":0}'.encode())
+        data = self.cam_socket.recv(512).decode()
+        while not "rval" in data:
+            time.sleep(0.1)
+            data = self.cam_socket.recv(512).decode()
+        self.cam_token = re.findall('"param": (.+) }',data)[0]
+        self._isconnected = True
+        
+    def set_datetime(self, a_datetime = strftime("%Y-%m-%d %H:%M:%S", localtime())):
+        if self._isconnected:
+            tosend = '{{"msg_id":2,"token":{}, "type":"camera_clock", "param":"' %self.cam_token + a_datetime + '"}'
+            self.cam_socket.send(tosend.encode())
+            self.cam_socket.recv(512)
+        else:
+            return "Not connected"
+            
+    def get_datetime(self):
+        if self._isconnected:
+            tosend = '{"msg_id":3,"token":%s}' %self.cam_token
+            self.cam_socket.send(tosend.encode())
+            while 1:
+                conf = self.cam_socket.recv(4096).decode()
+                if "param" in conf:
+                    break
+            conf = conf[37:]
+            return conf[3:40]
+        else:
+            return "Not connected"
+        
+        
+    def disconnect(self):
+        self.cam_socket.close()
+        self._isconnected = False
+        
+    def isconnected(self):
+        if self._isconnected = True:
+            return True
+        else:
+            return False
 
