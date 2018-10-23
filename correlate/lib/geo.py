df2ea6c17ae62fac367be7166823f9de13a6e87e
Stefal <stephane.peneau@wanadoo.fr>
2018-10-23 10:32:29 +0200

    add condition on elevation interpolation
    When gpx are manually edited, elevation is sometime missing. In this
    case, after[3] could be "None".
    This commit add the condition to not have a float + None operand.

--- correlate/lib/geo.py
+++ correlate/lib/geo.py
@@ -203,7 +203,7 @@ def interpolate_lat_lon(points, t, max_dt=1):
 
     bearing = compute_bearing(before[1], before[2], after[1], after[2])
 
-    if before[3] is not None:
+    if before[3] is not None and after[3] is not None:
         ele = (before[3] * dt_after + after[3] *
                dt_before) / (dt_before + dt_after)
     else:
